b41475292e9eb8cfd945f83a532e983b1b545f78
Stoyama <Stoyama@MacBook.local>
2021-01-12 09:28:56 +0900

    5. user 詳細表示機能 『show』 を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    # Strong Parameter の記述
     User.create(user_params)
   end
 
