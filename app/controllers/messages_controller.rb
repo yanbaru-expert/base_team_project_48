9042133ecf6d8a1051c8f874479a9a692ba8c5b1
kakitomo235 <59069977+kakitomo235@users.noreply.github.com>
2021-01-17 09:16:44 +0900

    詳細機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,16 @@ def new
     @message = Message.new
   end
 def index
-    @message =Message.all
+    @messages =Message.all
+end
+def show
+   @message = Message.find(params[:id])
 end
 
   def create
     Message.create(message_params)
   end
+
 private
     def message_params
         params.require(:message).permit(:title,:Contents)
