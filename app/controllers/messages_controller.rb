ce35d6854258a970b1adfad16e60bb8dd693053a
kakitomo235 <59069977+kakitomo235@users.noreply.github.com>
2021-01-17 09:40:45 +0900

    編集、更新機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,14 @@ end
   def create
     Message.create(message_params)
   end
+def edit
+   @message = Message.find(params[:id])
+end
 
+def update
+    message = Message.find(params[:id])
+    message.update(message_params)
+end
 private
     def message_params
         params.require(:message).permit(:title,:Contents)
